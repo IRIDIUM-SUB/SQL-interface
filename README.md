443f37f46ebebe66b1bd02ab7386799a63c45ca4
I-Hisen <POTASSIUM2045236112@outlook.com>
2019-12-11 00:35:25 +0800

    Login page's layout

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+<!--
+ * @Description:README
+ * @Date: 2019-12-10 21:46:56
+ * @Author: I-Hsien
+ * @LastEditors: I-Hsien
+ * @LastEditTime: 2019-12-10 22:39:34
+ -->
 # SQL-interface
 Charpter4 of database labtory,HUST. Use kivy as GUI interface.
+## Requirement
+1. 新生入学信息增加，学生信息修改。
+2. 课程信息维护（增加新课程，修改课程信息，删除没有选课的课程信息）。
+3. 录入学生成绩，修改学生成绩。
+4. 按系统计学生的平均成绩、最好成绩、最差成绩、优秀率、不及格人数。
+5. 按系对学生成绩进行排名，同时显示出学生、课程和成绩信息。
+6. 输入学号，显示该学生的基本信息和选课信息。
--- main.py
+++ main.py
@@ -3,13 +3,27 @@
 @Date: 2019-12-10 21:49:31
 @Author: I-Hsien
 @LastEditors: I-Hsien
-@LastEditTime: 2019-12-10 21:59:23
+@LastEditTime: 2019-12-11 00:20:52
 '''
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.image import AsyncImage
+Builder.load_file('layout.kv')
+class ScreenManager(ScreenManager):
+    pass
+
+class HomeScreen(Screen):
+    pass
+
+class LoginScreen(Screen):
+    pass
 
 class TestApp(App):
     def build(self):
-        return Button(text='Hello World')
+        #Config.set('graphics', 'width', '200')
+        #Config.set('graphics', 'height', '200')#Remove in mobile?
+        return ScreenManager()
 
 TestApp().run()
